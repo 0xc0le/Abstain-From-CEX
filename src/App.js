409da0f5d879fe39564d7d4bb2b2983bc7c1842d
0xc0le <cole511@protonmail.com>
2022-11-12 09:11:47 -0500

    worked on adding footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/navbar";
-import Header from "./components/header";
-import History from "./components/history";
+// import Header from "./components/header";
+// import History from "./components/history";
+import Footer from "./components/footer";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Header />
-      <History />
+      {/* <Header />
+      <History /> */}
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./footer.css";
+
+export default class footer extends Component {
+  render() {
+    return (
+      <div>
+        <footer className="app-footer">
+          If we do not learn from history, we are doomed to repeat it
+        </footer>
+      </div>
+    );
+  }
+}
