62f600bc8e74522094a7d04730eebd4c5b7205c8
0xc0le <cole511@protonmail.com>
2022-11-21 19:23:54 -0500

    Idk tbh

--- src/pages/alternatives/alternatives.js
+++ src/pages/alternatives/alternatives.js
@@ -1,11 +1,24 @@
 import React, { Component } from "react";
 import "./alternatives";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export default class alternatives extends Component {
   render() {
     return (
       <div className="page">
-        <div className="text">In Progress...</div>
+        <div className="cexed-header">
+          <Container>
+            <Row className="cexed-header-row">
+              <Col className="cexed-header-columns">
+                <div className="cexed-header-title">SAFE CEX ALTERNATIVES</div>
+                <div className="cexed-header-subtitle">Lorum Ipsum</div>
+              </Col>
+              <Col></Col>
+            </Row>
+          </Container>
+        </div>
       </div>
     );
   }
--- src/pages/cexed/cexed.js
+++ src/pages/cexed/cexed.js
@@ -1,65 +1,97 @@
 import React, { Component } from "react";
-import Accordion from "react-bootstrap/Accordion";
 import "./cexed.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export default class cexed extends Component {
   render() {
     return (
-      <div className="page">
-        <div className="cexed-header">CEX Ed</div>
+      <div className="cexed-page">
         <div className="cexed-header">
-          Self-custody can be dauting, but it doesn't have to be. By learning a
-          few basic topics and best practices, you can be well on your way to
-          becoming independent of centralized intermediaries
+          <Container>
+            <Row className="cexed-header-row">
+              <Col className="cexed-header-columns">
+                <div className="cexed-header-title">CEX ED</div>
+                <div className="cexed-header-subtitle">
+                  Self-custody can seem overwhelming, but by learning a few
+                  basic topics and best practices, you can be well on your way
+                  to becoming independent of centralized intermediaries
+                </div>
+              </Col>
+              <Col></Col>
+            </Row>
+          </Container>
         </div>
-        <Accordion className="accordian" defaultActiveKey="0">
-          <Accordion.Item eventKey="0">
-            <Accordion.Header>What is a CEX?</Accordion.Header>
-            <Accordion.Body>
-              A CEX, or Centralized Exchange, is... A CEX has full control of
-              your digital assets.
-            </Accordion.Body>
-          </Accordion.Item>
-          <Accordion.Item eventKey="0">
-            <Accordion.Header>
-              Where does a CEX keep your funds?
-            </Accordion.Header>
-            <Accordion.Body>
-              Some CEXs do not share where they keep client funds. Some lend
-              them out (ahem FTX)...
-            </Accordion.Body>
-          </Accordion.Item>
-          <Accordion.Item eventKey="0">
-            <Accordion.Header>What is a wallet?</Accordion.Header>
-            <Accordion.Body>
+        <Container className="educational-content-section">
+          <Row>
+            <div className="educational-headers">
+              What is a Centralized Exchange?
+            </div>
+            <div className="educational-content">
+              A CEX, or Centralized Exchange is a...
+              <div>
+                Examples of centralized exchanges include: Coinbase, Gemini,
+                Binance, and Bybit.
+              </div>
+            </div>
+          </Row>
+          <Row>
+            <div className="educational-headers">
+              What is a Self-Custody Wallet?
+            </div>
+            <div className="educational-content">
               A wallet is an account on the blockchain. This account is
               identified by a "public key" and protected by a "private key".
               These keys act like a username and password for the wallet and are
               generated using a type of math called cryptography.
-            </Accordion.Body>
-          </Accordion.Item>
-          <Accordion.Item eventKey="0">
-            <Accordion.Header>What does "self-custody" mean?</Accordion.Header>
-            <Accordion.Body>
-              Self-custody means owning your own funds. "Not your keys, not your
-              coins"
-            </Accordion.Body>
-          </Accordion.Item>
-          <Accordion.Item eventKey="0">
-            <Accordion.Header>Are wallets secure?</Accordion.Header>
-            <Accordion.Body>Yes, unless you own a Bored Ape</Accordion.Body>
-          </Accordion.Item>
-          <Accordion.Item eventKey="0">
-            <Accordion.Header>What is a seed phrase?</Accordion.Header>
-            <Accordion.Body>
-              A seed phrase is a 12 or 24 word blah blah blah...
-            </Accordion.Body>
-          </Accordion.Item>
-          <Accordion.Item eventKey="0">
-            <Accordion.Header>Blah blah blah?</Accordion.Header>
-            <Accordion.Body>Lorum Ipsum</Accordion.Body>
-          </Accordion.Item>
-        </Accordion>
+            </div>
+          </Row>
+          <Row>
+            <div className="educational-headers">
+              What is a Decentralized Exchange?
+            </div>
+            <div className="educational-content">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+              elementum, purus non lacinia fermentum, metus mauris pulvinar
+              massa, at rhoncus purus purus at orci. Vestibulum eget mi ut nisi
+              fringilla feugiat vel id leo. Suspendisse sagittis lobortis orci a
+              tempor. Maecenas id fringilla diam. Suspendisse non vulputate
+              ipsum. Nulla dignissim justo vitae urna cursus ultricies. Nunc
+              quis bibendum sapien. Phasellus porttitor gravida elit, a mollis
+              purus placerat nec. Donec nec tortor et ante aliquet fringilla eu
+              lacinia quam. Phasellus eleifend nunc in pharetra porttitor.
+            </div>
+          </Row>
+          <Row>
+            <div className="educational-headers">Lorum Ipsum</div>
+            <div className="educational-content">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+              elementum, purus non lacinia fermentum, metus mauris pulvinar
+              massa, at rhoncus purus purus at orci. Vestibulum eget mi ut nisi
+              fringilla feugiat vel id leo. Suspendisse sagittis lobortis orci a
+              tempor. Maecenas id fringilla diam. Suspendisse non vulputate
+              ipsum. Nulla dignissim justo vitae urna cursus ultricies. Nunc
+              quis bibendum sapien. Phasellus porttitor gravida elit, a mollis
+              purus placerat nec. Donec nec tortor et ante aliquet fringilla eu
+              lacinia quam. Phasellus eleifend nunc in pharetra porttitor.
+            </div>
+          </Row>
+          <Row>
+            <div className="educational-headers">Lorum Ipsum</div>
+            <div className="educational-content">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+              elementum, purus non lacinia fermentum, metus mauris pulvinar
+              massa, at rhoncus purus purus at orci. Vestibulum eget mi ut nisi
+              fringilla feugiat vel id leo. Suspendisse sagittis lobortis orci a
+              tempor. Maecenas id fringilla diam. Suspendisse non vulputate
+              ipsum. Nulla dignissim justo vitae urna cursus ultricies. Nunc
+              quis bibendum sapien. Phasellus porttitor gravida elit, a mollis
+              purus placerat nec. Donec nec tortor et ante aliquet fringilla eu
+              lacinia quam. Phasellus eleifend nunc in pharetra porttitor.
+            </div>
+          </Row>
+        </Container>
       </div>
     );
   }
