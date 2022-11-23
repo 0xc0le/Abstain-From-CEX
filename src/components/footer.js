979815219b729f43a1468d4573d2d4e8222e6c93
0xc0le <cole511@protonmail.com>
2022-11-22 22:34:54 -0500

    Idk tbh

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,102 +1,17 @@
 import React from "react";
-import { MDBFooter, MDBContainer, MDBCol, MDBRow } from "mdb-react-ui-kit";
+import { MDBFooter, MDBContainer } from "mdb-react-ui-kit";
 import "./headerfooter.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
 
 export default function App() {
   return (
     <MDBFooter className="text-center" color="white">
       <MDBContainer className="p-4">
-        <section className="">
-          <MDBRow>
-            <MDBCol lg="3" md="6" className="mb-4 mb-md-0">
-              <h5 className="text-uppercase">About</h5>
-
-              <ul className="list-unstyled mb-0">
-                <li>
-                  <a href="#!" className="text-white">
-                    Link 1
-                  </a>
-                </li>
-                <li>
-                  <a href="#!" className="text-white">
-                    Link 2
-                  </a>
-                </li>
-                <li>
-                  <a href="#!" className="text-white">
-                    Link 3
-                  </a>
-                </li>
-              </ul>
-            </MDBCol>
-
-            <MDBCol lg="3" md="6" className="mb-4 mb-md-0">
-              <h5 className="text-uppercase">Social</h5>
-
-              <ul className="list-unstyled mb-0">
-                <li>
-                  <a href="#!" className="text-white">
-                    Link 1
-                  </a>
-                </li>
-                <li>
-                  <a href="#!" className="text-white">
-                    Link 2
-                  </a>
-                </li>
-                <li>
-                  <a href="#!" className="text-white">
-                    Link 3
-                  </a>
-                </li>
-              </ul>
-            </MDBCol>
-
-            <MDBCol lg="3" md="6" className="mb-4 mb-md-0">
-              <h5 className="text-uppercase">Lorem Ipsum</h5>
-
-              <ul className="list-unstyled mb-0">
-                <li>
-                  <a href="#!" className="text-white">
-                    Link 1
-                  </a>
-                </li>
-                <li>
-                  <a href="#!" className="text-white">
-                    Link 2
-                  </a>
-                </li>
-                <li>
-                  <a href="#!" className="text-white">
-                    Link 3
-                  </a>
-                </li>
-              </ul>
-            </MDBCol>
-
-            <MDBCol lg="3" md="6" className="mb-4 mb-md-0">
-              <h5 className="text-uppercase">Other Resources</h5>
-
-              <ul className="list-unstyled mb-0">
-                <li>
-                  <a href="#!" className="text-white">
-                    Link 1
-                  </a>
-                </li>
-                <li>
-                  <a href="#!" className="text-white">
-                    Link 2
-                  </a>
-                </li>
-                <li>
-                  <a href="#!" className="text-white">
-                    Link 3
-                  </a>
-                </li>
-              </ul>
-            </MDBCol>
-          </MDBRow>
-        </section>
+        <div className="footer-text">
+          Made with <FontAwesomeIcon icon={faHeart} color="#9C7506" /> and
+          caffiene by 0xc0le.ETH
+        </div>
       </MDBContainer>
     </MDBFooter>
   );
--- src/pages/cexed/cexed.js
+++ src/pages/cexed/cexed.js
@@ -29,11 +29,21 @@ export default class cexed extends Component {
               What is a Centralized Exchange?
             </div>
             <div className="educational-content">
-              A CEX, or Centralized Exchange is a...
-              <div>
-                Examples of centralized exchanges include: Coinbase, Gemini,
-                Binance, and Bybit.
-              </div>
+              A CEX, or Centralized Exchange, is a type of cryptocurrency
+              exchange that accepts fiat or crypto from a user and facilitates
+              trades on behalf of the user. CEXs have full control of users'
+              funds and sometimes lend out those funds without the user's
+              consent. This can be problematic because CEXs may lose customer
+              funds in speculative investments. CEXs could also not have enough
+              funds on hand to cover customer withdrawls. These situations
+              typically all end the same, with the company declaring bankruptcy.
+              When a CEX declares bankruptcy, customers are treated as creditors
+              and generally recieve pennies on the dollar after years of
+              bankruptcy proceedings. This is why the phrase, "not your keys,
+              not your coins" is so popular!
+              <br /> <br />
+              Examples of centralized exchanges include: Coinbase, Gemini,
+              Binance, OKX, and Bybit.
             </div>
           </Row>
           <Row>
@@ -52,15 +62,11 @@ export default class cexed extends Component {
               What is a Decentralized Exchange?
             </div>
             <div className="educational-content">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-              elementum, purus non lacinia fermentum, metus mauris pulvinar
-              massa, at rhoncus purus purus at orci. Vestibulum eget mi ut nisi
-              fringilla feugiat vel id leo. Suspendisse sagittis lobortis orci a
-              tempor. Maecenas id fringilla diam. Suspendisse non vulputate
-              ipsum. Nulla dignissim justo vitae urna cursus ultricies. Nunc
-              quis bibendum sapien. Phasellus porttitor gravida elit, a mollis
-              purus placerat nec. Donec nec tortor et ante aliquet fringilla eu
-              lacinia quam. Phasellus eleifend nunc in pharetra porttitor.
+              A DEX, or decentralized exchange, is a type of cryptocurrency
+              exchange that allows users to trade tokens in a peer-to-peer
+              manner. This type of exchange is made possible by smart contracts.
+              <br /> <br /> Examples of decentralized exchanges include:
+              UniSwap, SushiSwap, and Curve.
             </div>
           </Row>
           <Row>
