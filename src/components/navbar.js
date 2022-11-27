6da28976c63901a80c61b7e37ce158b503170647
0xc0le <cole511@protonmail.com>
2022-11-27 16:05:09 -0500

    wip

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,21 +6,23 @@ import Cexed from "../pages/cexed/cexed";
 import Alternatives from "../pages/alternatives/alternatives";
 import Merch from "../pages/merch/merch";
 import Home from "../pages/home/home";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Container from "react-bootstrap/Container";
 
 export default class navbar extends Component {
   render() {
     return (
       <Router>
-        <div>
-          <Navbar className="nav">
-            <Navbar.Brand
-              as={Link}
-              to={"/"}
-              className="navbar-brand"
-              href="#home"
-            >
-              ABSTAIN FROM CEX
-            </Navbar.Brand>
+        {/* <Navbar className="nav" expand="lg">
+          <Navbar.Brand
+            as={Link}
+            to={"/"}
+            className="navbar-brand"
+            href="#home"
+          >
+            ABSTAIN FROM CEX
+          </Navbar.Brand>
+          <Navbar.Collapse>
             <Nav className="NavLinks">
               <Nav.Link
                 as={Link}
@@ -47,8 +49,44 @@ export default class navbar extends Component {
                 Protection (Merch)
               </Nav.Link>
             </Nav>
-          </Navbar>
-        </div>
+          </Navbar.Collapse>
+        </Navbar> */}
+        <Navbar bg="#1f2833" expand="lg" variant="dark">
+          <Container>
+            <Navbar.Brand as={Link} to={"/"} href="#home">
+              ABSTAIN FROM CEX
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ms-auto">
+                <Nav.Link
+                  as={Link}
+                  to={"/cexed"}
+                  href="#cexed"
+                  className="NavLinks"
+                >
+                  CEX Ed
+                </Nav.Link>
+                <Nav.Link
+                  as={Link}
+                  to={"/alternatives"}
+                  href="#alternatives"
+                  className="NavLinks"
+                >
+                  Safe CEX Alternatives
+                </Nav.Link>
+                <Nav.Link
+                  as={Link}
+                  to={"/merch"}
+                  href="#merch"
+                  className="NavLinks"
+                >
+                  Protection (Merch)
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cexed" element={<Cexed />} />
